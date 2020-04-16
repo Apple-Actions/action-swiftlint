cbd796a466cd97238b062b8e6b951614bfdca27f
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-04-16 17:04:17 +1000

    Fill out entrypoint script to run swiftlint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+set -e
+
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+  cd "${GITHUB_WORKSPACE}" || exit
+fi
+
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+swiftlint \
+  | reviewdog -efm="%f:%l:%c: %m" -name="swiftlint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
